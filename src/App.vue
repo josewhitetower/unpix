c5cf6468347d872f8c0100470a51ade9810f2445
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-07-30 21:59:31 +0200

    added project setup

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Unpix</h1>
+    <form @submit.prevent="onSearch">
+      <input type="text" v-model="query" />
+    </form>
+    <button @click="onRandom">random</button>
+  </div>
+</template>
+
+<script>
+import unplash from './api/unplash';
+export default {
+  name: 'App',
+  data: () => ({
+    query: '',
+    page: 1,
+  }),
+  components: {},
+  methods: {
+    async onSearch() {
+      const query = `query=${this.query}&page=${this.page}`
+      const data = await unplash.search(query);
+      if (data) {
+        console.log(data);
+        this.query = ''
+      }
+    },
+    async onRandom() {
+      const data = await unplash.random()
+      if (data) {
+        console.log(data)
+      }
+    }
+  },
+};
+</script>
+
+<style></style>
---
+++ src/api/unplash.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+require('dotenv').config({path: '../.env'});
+const instance = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  headers: {
+    'Accept-Version': 'v1',
+    Authorization: `Client-ID ${process.env.VUE_APP_CLIENT_ID}`,
+  },
+});
+
+const unplash = {
+  search: (query) => {
+    const path = `/search/photos?${query}`;
+    return instance
+      .get(path)
+      .then((response) => response.data)
+      .catch((error) => ({error: error}));
+  },
+  random: () => {
+    const path = `/photos/random`;
+    return instance
+      .get(path)
+      .then((response) => response.data)
+      .catch((error) => ({error: error}));
+  },
+};
+
+module.exports = unplash;
