2c7f0e5ae8e496299c54bb123ad5b6451469d029
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-07-31 14:52:07 +0200

    added photo card component

---
+++ src/components/PhotoCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="mx-auto opacity-100 hover:opacity-75 cursor-pointer overflow-hidden rounded-sm">
+    <!-- Desktop Images -->
+    <img
+      class="hidden md:block object-cover h-64 w-64"
+      :src="getPhotoURLw242"
+      :alt="photo.alt"
+      loading="lazy"
+    />
+    <!-- Mobile Images -->
+    <img
+      class="md:hidden object-cover h-40 w-40"
+      :src="getPhotoURLw128"
+      :alt="photo.alt"
+      loading="lazy"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PhotoCard',
+  props: {
+    photo: {
+      required: true,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    getPhotoURLw242() {
+      return this.photo.url + '&w=242&dpr=2';
+    },
+    getPhotoURLw128() {
+      return this.photo.url + '&w=128&dpr=2';
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/components/PhotosGrid.vue
+++ src/components/PhotosGrid.vue
@@ -1,34 +1,20 @@
 <template>
   <div class="grid grid-cols-3 gap-1 md:gap-5 max-w-3xl mx-auto">
-    <!-- Desktop Images -->
-    <img
-      class="hidden md:block mx-auto opacity-100 hover:opacity-75 cursor-pointer h-40 w-full md:h-64 md:w-64 overflow-hidden object-cover rounded-sm"
-      v-for="photo in photos"
-      :key="getPhotoURLw242(photo.url)"
-      :src="getPhotoURLw242(photo.url)"
-      :alt="photo.alt"
-      loading="lazy"
-    />
-
-    <!-- Mobile Images -->
-    <img
-      class="md:hidden mx-auto opacity-100 hover:opacity-75 cursor-pointer h-40 w-full md:h-64 md:w-64 overflow-hidden object-cover rounded-sm"
-      v-for="photo in photos"
-      :key="getPhotoURLw128(photo.url)"
-      :src="getPhotoURLw128(photo.url)"
-      :alt="photo.alt"
-      loading="lazy"
-    />
+    <PhotoCard v-for="photo in photos" :key="photo.id" :photo="photo" />
   </div>
 </template>
 
 <script>
+import PhotoCard from './PhotoCard';
 export default {
   props: {
     photos: {
       required: true,
       default: () => [],
     },
+  },
+  components: {
+    PhotoCard,
   },
   methods: {
     getPhotoURLw242(url) {
