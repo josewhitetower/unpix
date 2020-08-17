f0b096162bff529a86bc6f468f7d02a4a803fa22
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-08-17 15:36:40 +0200

    reoved purge csdd

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
-const purgecss = require("@fullhuman/postcss-purgecss");
 module.exports = {
     plugins: [
         tailwindcss("./tailwind.config.js"),
@@ -8,12 +7,5 @@ module.exports = {
             add: true,
             grid: false
         }),
-        //Only add purgecss in production
-        process.env.NODE_ENV === "production"? purgecss({
-            content: [
-                "./src/**/*.html",
-                "./src/**/*.vue"
-            ]
-        }): ""
     ]
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,37 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require('tailwindcss/plugin');
 module.exports = {
-
-  purge: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: ['./src/**/*.html', './src/**/*.vue'],
+  },
   theme: {
     extend: {
       keyframes: {
-        'zoomIn': {
-          '0%': {transform:'scale(0)'},
-          '100%': {transform:'scale(1)'}
-        }
+        zoomIn: {
+          '0%': {transform: 'scale(0)'},
+          '100%': {transform: 'scale(1)'},
+        },
       },
       animation: {
-        'zoom-in': 'zoomIn 0.3s ease-in-out'
-      }
+        'zoom-in': 'zoomIn 0.3s ease-in-out',
+      },
     },
   },
   variants: {},
   plugins: [
-    plugin(({ addUtilities }) => {
+    plugin(({addUtilities}) => {
       const newUtilities = {
         '.bg-back': {
-          backgroundColor: 'rgba(0,0,0,0.3)'
+          backgroundColor: 'rgba(0,0,0,0.3)',
         },
-        '.blur' : {
-          filter: 'blur(4px)'
-        }
-      }
+        '.blur': {
+          filter: 'blur(4px)',
+        },
+      };
       addUtilities(newUtilities, {
-        variants: ['hover']
-      })
-    })
+        variants: ['hover'],
+      });
+    }),
   ],
-}
+};
